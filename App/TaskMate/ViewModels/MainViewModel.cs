bef60c36b4218d249eba565fb82cc942b82ef7b2
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-08 11:15:01 -0300

    Criação e injeção de MainViewModel.

---
+++ App/TaskMate/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using TaskMate.ViewModels.Tasks;
+
+namespace TaskMate.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            MyTasks = new MyTaskListViewModel();
+        }
+
+        public MyTaskListViewModel MyTasks { get; private set; }
+    }
+}
