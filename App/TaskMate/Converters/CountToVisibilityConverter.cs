ce3c5904e5a0bf3f9a22e4e589a6401b670a61d2
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-08 17:07:40 -0300

    Novo conversor CountToVisibility e novo estilo para ícones grandes.

---
+++ App/TaskMate/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace TaskMate.Converters
+{
+    public class CountToVisibilityConverter : IValueConverter
+    {
+        public bool ShouldBeEmpty { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!ShouldBeEmpty)
+            {
+                if (value is int count && count > 0) return Visibility.Visible;
+                else return Visibility.Collapsed;
+            }
+            else
+            {
+                if (value is int count && count == 0) return Visibility.Visible;
+                else return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
