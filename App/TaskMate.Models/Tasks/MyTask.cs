d4189ef5f1cb8368caf8112bb6d3838de3ce6850
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-08 16:25:26 -0300

    Adaptação modelo relacional.

--- App/TaskMate.Models/Tasks/MyTask.cs
+++ App/TaskMate.Models/Tasks/MyTask.cs
@@ -1,18 +1,20 @@
-﻿using TaskMate.Models.Tasks.Enums;
+﻿using TaskMate.Models.Common;
+using TaskMate.Models.Tasks.Enums;
 
 namespace TaskMate.Models.Tasks
 {
-    public class MyTask
+    public class MyTask : DbObject
     {
         public MyTask()
         {
+            ToDos.Add(new ToDo("Item 1"));
         }
 
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime DueDate { get; set; }  
         public bool IsCompleted { get; set; }
-        public List<TaskToDos> Observations { get; set; } = new List<TaskToDos>();
         public TaskPriority Priority { get; set; }
+        public List<ToDo> ToDos { get; set; } = new List<ToDo>();
     }
 }
---
+++ App/TaskMate.Models/Tasks/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using TaskMate.Models.Common;
+
+namespace TaskMate.Models.Tasks
+{
+    public class ToDo : DbObject
+    {
+        public ToDo(string? description)
+        {
+            Description = description;
+        }
+
+        public MyTask? MyTask { get; set; }  
+        public int MyTaskId { get; set; } 
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }   
+    }
+}
