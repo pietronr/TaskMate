15e9954467be6e847108cdd5fd52ed90b74f3b0e
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-08 16:01:06 -0300

    Comandos de editar e excluir MyTasks.

--- App/TaskMate/ViewModels/Tasks/MyTaskListViewModel.cs
+++ App/TaskMate/ViewModels/Tasks/MyTaskListViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using TaskMate.ViewModels.Helpers;
 using TaskMate.Views.Dialogs;
 
 namespace TaskMate.ViewModels.Tasks
@@ -16,6 +17,8 @@ namespace TaskMate.ViewModels.Tasks
             {
                 dialogManager = new Surfaces.DialogManager();
                 NewTaskCommand = new AsyncRelayCommand(NewTask);
+                EditTaskCommand = new AsyncRelayCommand<MyTaskViewModel>(EditTask);
+                RemoveTaskCommand = new RelayCommand<MyTaskViewModel>(RemoveTask);
             }
         } 
 
@@ -33,11 +36,15 @@ namespace TaskMate.ViewModels.Tasks
 
         public ICommand? NewTaskCommand { get; set; }
 
+        public ICommand? EditTaskCommand { get; set; }  
+
+        public ICommand? RemoveTaskCommand { get; set; }
+
         #endregion
 
         #region Methods
 
-        public async Task NewTask()
+        private async Task NewTask()
         {
             MyTaskViewModel newTask = new()
             {
@@ -45,6 +52,18 @@ namespace TaskMate.ViewModels.Tasks
             };
 
             await dialogManager!.ShowDialogAsync(newTask, new MyTasksDialog(), false, false);
+        }
+
+        private async Task EditTask(MyTaskViewModel task)
+        {
+            task.IsInEdit = true;   
+            await dialogManager!.ShowDialogAsync(task, new MyTasksDialog(), false, false);
+        }
+
+        private void RemoveTask(MyTaskViewModel task)
+        {
+            TasksList.Remove(task);
+            TasksList.RefreshCollection();
         }
 
         #endregion
