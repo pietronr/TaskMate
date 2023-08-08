a99c0d2a6e04f12b9c7dab448da324213b5913f1
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-08 14:14:03 -0300

    MainViewModel em App.

--- App/TaskMate/App.xaml.cs
+++ App/TaskMate/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using TaskMate.ViewModels;
 
 namespace TaskMate
 {
@@ -13,5 +8,6 @@ namespace TaskMate
     /// </summary>
     public partial class App : Application
     {
+        public static MainViewModel MainViewModel { get { return (MainViewModel)Current.Resources["MainViewModel"]; } }
     }
 }
