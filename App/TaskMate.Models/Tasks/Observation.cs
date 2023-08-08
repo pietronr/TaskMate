aa7e723443211a46f922387ea5c83a6968f6445c
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-08 11:06:19 -0300

    Correção Observation.

--- App/TaskMate.Models/Tasks/Observation.cs
+++ App/TaskMate.Models/Tasks/Observation.cs
@@ -2,6 +2,12 @@
 {
     public struct Observation
     {
+        public Observation(string? description)
+        {
+            Description = description;
+            IsCompleted = false;
+        }
+
         public string? Description { get; set; }
         public bool IsCompleted { get; set; }
     }
