d1c9a6b31dd58889c2f947123b6d6f39e4bef0cf
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-08 11:14:49 -0300

    MyTaskListViewModel finalizada

---
+++ App/TaskMate/ViewModels/Tasks/MyTaskListViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TaskMate.ViewModels.Helpers;
+
+namespace TaskMate.ViewModels.Tasks
+{
+    public class MyTaskListViewModel : BindableBase
+    {
+        public MyTaskListViewModel(bool initialze)
+        {
+            if (initialze)
+            {
+                NewTaskCommand = new RelayCommand(NewTask);
+            }
+        } 
+
+        public MyTaskListViewModel() : this(true)
+        {
+            CurrentTask = new MyTaskViewModel();
+        }
+
+        #region Properties
+
+        private MyTaskViewModel? _currentTask;
+        public MyTaskViewModel? CurrentTask
+        {
+            get => _currentTask;
+            set => Set(ref _currentTask, value);
+        }
+
+        public ObservableCollection<MyTaskViewModel> MyTasksList { get; set; } = new ObservableCollection<MyTaskViewModel>();
+
+        #endregion
+
+        #region Commands
+
+        public ICommand? NewTaskCommand { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public void NewTask()
+        {
+            MyTasksList.Add(CurrentTask!);
+            MyTasksList.RefreshCollection();
+            CurrentTask = new MyTaskViewModel();
+        }
+
+        #endregion
+    }
+}
