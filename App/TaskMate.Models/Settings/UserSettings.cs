8c8c8c82a733c5c60cca0ed742a97e958ee9b198
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-11 15:14:20 -0300

    Renomeando entidade.

---
+++ App/TaskMate.Models/Settings/UserSettings.cs
@@ -0,0 +1,28 @@
+﻿using TaskMate.Models.Common;
+using TaskMate.Models.Settings.Enums;
+
+namespace TaskMate.Models.Settings
+{
+    public class UserSettings : DbObject
+    {
+        public bool ReceiveNotifications { get; set; }
+        public int DaysBeforeTaskNotification { get; set; }
+        public bool RemindMeAtDate { get; set; }
+        public int HourOfTheDate { get; set; }  
+        public NotificationType Type { get; set; }
+
+        public UserSettings Duplicate()
+        {
+            UserSettings userSettings = new()
+            {
+                ReceiveNotifications = ReceiveNotifications,
+                DaysBeforeTaskNotification = DaysBeforeTaskNotification,
+                RemindMeAtDate = RemindMeAtDate,
+                HourOfTheDate = HourOfTheDate,
+                Type = Type
+            };
+
+            return userSettings;
+        }
+    }
+}
