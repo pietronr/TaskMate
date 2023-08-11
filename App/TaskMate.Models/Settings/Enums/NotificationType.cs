68a39fb426df45ac607f3c1108882848561c71c6
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-08-11 14:48:04 -0300

    Entidade para configurações do usuário.

---
+++ App/TaskMate.Models/Settings/Enums/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace TaskMate.Models.Settings.Enums
+{
+    public enum NotificationType
+    {
+        [Description("Simples")]
+        Simple = 0,
+        [Description("Moderada")]
+        Moderate,
+        [Description("Complexa")]
+        Complex
+    }
+}
---
+++ App/TaskMate.Models/Settings/Settings.cs
@@ -0,0 +1,14 @@
+﻿using TaskMate.Models.Common;
+using TaskMate.Models.Settings.Enums;
+
+namespace TaskMate.Models.Settings
+{
+    public class Settings : DbObject
+    {
+        public bool ReceiveNotifications { get; set; }
+        public int DaysBeforeTaskNotification { get; set; }
+        public bool RemindMeAtDate { get; set; }
+        public int HourOfTheDate { get; set; }  
+        public NotificationType Type { get; set; }
+    }
+}
